0a81b997d240d4c38ea7cc1fa7264568432c8973
Mahadevan Iyer <mahadevan.n.iyer@gmail.com>
2025-11-04 20:43:41 +0530

    Removed advertisings

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -4,7 +4,7 @@ import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
-  links: Record<string, string>
+  links?: Record<string, string>
 }
 
 export default ((opts?: Options) => {
@@ -13,10 +13,6 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
